66e444bbe9348cee4850f74bc10fd88fa9c46da8
Storit-k <egorkostr@gmail.com>
2025-10-18 00:44:46 +0300

    Database fixed

--- back/app/core/database.py
+++ back/app/core/database.py
@@ -1,4 +1,4 @@
 from sqlmodel import create_engine
 
 
-engine = create_engine("sqlite:///back/data.db", connect_args={"check_same_thread": False})
+engine = create_engine("sqlite:///data.db", connect_args={"check_same_thread": False})
