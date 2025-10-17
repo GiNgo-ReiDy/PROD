ad006a131553de50ded01c1da4bbbcc001f2be99
GiNgo-ReiDy <protstomalchik15@gmail.com>
2025-10-17 18:55:50 +0300

    pre-React

--- back/main.py
+++ back/main.py
@@ -1,15 +1,22 @@
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 import uvicorn
 
-app = FastAPI()
+app = FastAPI() 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.on_event("startup")
-def strartup():
+def startup():
     ...
-
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
