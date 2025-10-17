e71e111f92ad79574ccca52e83af9be2c9d5917d
Storit-k <egorkostr@gmail.com>
2025-10-17 17:34:51 +0300

    nsiu hgiawuye gfiYEFG

--- back/main.py
+++ back/main.py
@@ -1,16 +1,23 @@
 from fastapi.responses import HTMLResponse
 from fastapi import FastAPI
 
+from sqlmodel import SQLModel
+
+from back.app.core import engine
+
 import uvicorn
 
 app = FastAPI()
 
 
-app.on_event("startup")
+@app.on_event("startup")
 def strartup():
-    ...
+    SQLModel.metadata.create_all(bind=engine)
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
     return "Hello world!"
+
+
+uvicorn.run(app, host="0.0.0.0", port=8000)
