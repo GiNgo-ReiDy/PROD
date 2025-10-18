ee9ef6c48efb14e8aaaf8d050e282ad917ee82d8
Storit-k <egorkostr@gmail.com>
2025-10-18 13:31:07 +0300

    Login Hotfix

--- back/app/api/routes/auth_route.py
+++ back/app/api/routes/auth_route.py
@@ -1,21 +1,17 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from back.app.core.security import hash_password, create_jwt
+from back.app.api.deps import LoginDep
 from back.app.models import Token
 
 router = APIRouter(prefix='/auth')
 
-class LoginRequest(BaseModel):
-    username: str
-    password: str
 
 @router.post('/login', response_model=Token)
-def user_login(user: LoginRequest):
-    # Пример логики
-    hashed_pwd = hash_password(user.password)
+def user_login(user: LoginDep):
     data = {
-        'sub': user.username,
-        'pwd': hashed_pwd.hex(),
+        'sub': user.uuid,
+        'pwd': user.password_hash.hex(),
     }
     jwt = create_jwt(data)
     return Token(access_token=jwt)
