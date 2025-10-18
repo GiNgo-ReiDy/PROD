0bf763700677c82bf3cf61838e951fe118d344eb
Storit-k <egorkostr@gmail.com>
2025-10-18 03:36:38 +0300

    Added user managment interactions

--- back/app/api/routes/users_route.py
+++ back/app/api/routes/users_route.py
@@ -1,4 +1,67 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Depends, Body, status
+
+from typing import Annotated
+from sqlmodel import select
+
+from back.app.models.users_models import User, UserGetWithGroup, UserPost, UserPatch
+from back.app.api.deps import SessionDep, UserDep, AdminDep
+from back.app.models.groups_models import Group
+from back.app.core.security import hash_password
 
 router = APIRouter(prefix = '/users')
+
+
+@router.get('/', response_model=list[UserGetWithGroup])
+async def get_users(session: SessionDep):
+    users = session.exec(select(User)).all()
+
+    if not users:
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
+
+    return users
+
+
+@router.post('/', response_model=UserGetWithGroup, status_code=status.HTTP_201_CREATED)
+async def create_user(data: Annotated[UserPost, Depends()], session: SessionDep, admin: AdminDep):
+    user = User.model_validate(data, update={'password_hash': hash_password(data.password)})
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+@router.patch('/', response_model=UserGetWithGroup)
+async def update_user(data: Annotated[UserPatch, Depends()], session: SessionDep, user: UserDep):
+    target = session.get(User, data.uuid)
+
+    if not target:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+    if target.uuid != user.uuid and (user.category != 2 or target.category == 2):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+    data_dict = data.model_dump(exclude_none=True)
+
+    if data.group_id:
+        new_group = session.get(Group, data.group_id)
+        if not new_group:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Group not found')
+        data_dict.update({'group': new_group})
+
+    target.sqlmodel_update(data_dict)
+    session.add(target)
+    session.commit()
+    session.refresh(target)
+    return target
+
+
+@router.delete('/', response_model=UserGetWithGroup)
+def delete_user(session: SessionDep, admin: AdminDep, uuid: str = Body(embed=True)):
+    user = session.get(User, uuid)
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+
+    session.delete(user)
+    session.commit()
+    return {'detail': 'OK'}
 
--- back/app/models/users_models.py
+++ back/app/models/users_models.py
@@ -1,33 +1,48 @@
 from sqlmodel import SQLModel, Field, Relationship
+from pydantic import BaseModel
 from uuid import uuid4
 
 from .groups_models import Group, GroupGetWithUsers, GroupGet
 from .sessions_models import Session, SessionGet
 
 
-class BaseUser(SQLModel):
+class UserBase(SQLModel):
     login: str
     category: int
 
 
-class User(BaseUser, table=True):
+class User(UserBase, table=True):
     uuid: str | None = Field(primary_key=True, default_factory=lambda: str(uuid4()))
     group_id: int | None = Field(default=None, foreign_key="groups.id")
     password_hash: bytes
 
-    group: Group = Relationship(back_populates="users")
+    group: Group | None = Relationship(back_populates="users")
     session: Session | None = Relationship(back_populates="user")
 
     __tablename__ = 'users'
 
 
-class UserGet(BaseUser):
+class UserGet(UserBase):
     uuid: str
-    session: SessionGet
+    session: SessionGet | None = None
 
 
 class UserGetWithGroup(UserGet):
-    group: GroupGet
+    group: GroupGet | None = None
+
+
+class UserPost(UserBase):
+    password: str
+    group_id: int | None = None
+
+
+class UserPatch(BaseModel):
+    uuid: str
+    login: str | None = None
+    password: str | None = None
+    group_id: int | None = None
+    category: int | None = None
+
 
 
 GroupGetWithUsers.model_rebuild()
