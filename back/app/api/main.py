52902f574f6288e306250ba90ce0057bfcdb1669
dibag542 <di_bag_09@mail.ru>
2025-10-18 13:35:53 +0300

    sessions route tested

--- back/app/api/main.py
+++ back/app/api/main.py
@@ -1,4 +1,4 @@
-from .routes import auth_router, users_router, groups_router
+from .routes import auth_router, users_router, groups_router, sessions_router
 from fastapi import FastAPI, APIRouter
 
 
@@ -7,3 +7,4 @@ api_router = APIRouter(prefix='/api')
 api_router.include_router(auth_router)
 api_router.include_router(users_router)
 api_router.include_router(groups_router)
+api_router.include_router(sessions_router)
--- back/app/api/routes/__init__.py
+++ back/app/api/routes/__init__.py
@@ -1,4 +1,5 @@
 from .users_route import router as users_router
 from .groups_route import router as groups_router
 from .auth_route import router as auth_router
+from .sessions_route import router as sessions_router
 
